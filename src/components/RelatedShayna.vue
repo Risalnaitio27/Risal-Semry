c4a602005c8e9adca5348ea4fe4b4f64c9e882ab
Risalnaitio27 <naitiorisal@gmail.com>
2023-01-30 12:30:37 +0800

    first commit

---
+++ src/components/RelatedShayna.vue
@@ -0,0 +1,269 @@
+<template>
+  <!-- Related Products Section End -->
+  <div class="related-products spad">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-12">
+          <div class="section-title">
+            <h2> Produk Terlaris</h2>
+            <h2> Harga Kaki Lima Kualitas Bintang Lima</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-1.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <router-link to="/product">+ Quick View</router-link>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Coat</div>
+              <a href="#">
+                <h5>Kaos Pria</h5>
+              </a>
+              <div class="product-price">
+                Rp,-14.000
+                <span>$35.00</span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-2.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Shoes</div>
+              <a href="#">
+                <h5>Kemeja Wanita</h5>
+              </a>
+              <div class="product-price">Rp,-13.000</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-3.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Towel</div>
+              <a href="#">
+                <h5>Rok Wanita</h5>
+              </a>
+              <div class="product-price">Rp,-34.000</div>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-4.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Towel</div>
+              <a href="#">
+                <h5>Rok Wanita</h5>
+              </a>
+              <div class="product-price">Rp,-40.000</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-5.png" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Towel</div>
+              <a href="#">
+                <h5>Rok Wanita</h5>
+              </a>
+              <div class="product-price">Rp,-40.000</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-6.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Towel</div>
+              <a href="#">
+                <h5>Kemeja Pria</h5>
+              </a>
+              <div class="product-price">Rp,-40.000</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-7.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Towel</div>
+              <a href="#">
+                <h5>Kemeja Pria</h5>
+              </a>
+              <div class="product-price">Rp,-40.000</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-8.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Towel</div>
+              <a href="#">
+                <h5>Rok Wanita</h5>
+              </a>
+              <div class="product-price">Rp,-40.000</div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-9.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Towel</div>
+              <a href="#">
+                <h5>Rok Wanita</h5>
+              </a>
+              <div class="product-price">Rp,-40.000</div>
+            </div>
+          </div>
+        </div>
+  
+        <!-- <div class="col-lg-3 col-sm-6">
+          <div class="product-item">
+            <div class="pi-pic">
+              <img src="img/products/women-4.jpg" alt />
+              <ul>
+                <li class="w-icon active">
+                  <a href="#">
+                    <i class="icon_bag_alt"></i>
+                  </a>
+                </li>
+                <li class="quick-view">
+                  <a href="#">+ Quick View</a>
+                </li>
+              </ul>
+            </div>
+            <div class="pi-text">
+              <div class="catagory-name">Towel</div>
+              <a href="#">
+                <h5>Rok Wanita</h5>
+              </a>
+              <div class="product-price">Rp,-40.000</div>
+            </div>
+          </div>
+        </div> lang -->
+      </div> 
+    </div>
+  </div>
+  <!-- Related Products Section End -->
+</template>
+
+<script>
+export default {
+    name: "RelatedShayna"
+}
+</script>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,48 @@
+<!-- <template>
+        <v-card>
+        <img class="logo" src="../assets/but.webp" />
+        <h1>Sign Up</h1>
+        <div class="register" >
+                <input type="text" placeholder="enter name" />
+                <input type="text" placeholder="enter email" />
+                <input type="password" placeholder="enter password" />
+                <button>Sign Up</button>
+        </div>
+     
+
+
+</v-card>    
+</template>
+
+
+<script>
+
+export default {
+        name : 'SignUp'
+}
+</script>
+
+<style>
+.logo{
+        width:100px
+}
+.register input{
+        width: 300px;
+        height:40px;
+        padding-left: 20px;
+        display: block;
+        margin-bottom: 30px;
+        margin-right: auto;
+        margin-left: auto;
+        border: 1px solid skyblue;
+}
+.register button{
+        width: 320px;
+        height:40px; 
+        border: 1px solid skyblue; 
+        background: skyblue; 
+        color: #fff;
+        cursor: pointer;
+
+}
+</style> -->
